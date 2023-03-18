bab030af5b28a800f0251204e5691b3de059b7db
hqphat <hqphat203@gmail.com>
2023-03-18 15:37:09 +0700

    update

---
+++ Baithuchanh0303/src/Slide55.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Slide55 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i <= 200; i++) {
+            if (i % 5 != 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+
+    }
+
+}
